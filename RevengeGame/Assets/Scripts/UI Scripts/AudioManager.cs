d612d66f40cb1bea95555f35235156fc12d76671
Fuss360 <djtaliaferro@my.waketech.edu>
2023-10-01 12:13:03 -0400

    Music UI Updates

--- RevengeGame/Assets/Scripts/UI Scripts/AudioManager.cs
+++ RevengeGame/Assets/Scripts/UI Scripts/AudioManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
@@ -8,6 +10,25 @@ public class AudioManager : MonoBehaviour
 
     [SerializeField] private AudioClip[] audioClips;
     [SerializeField] private AudioSource audioSource;
+    public AudioMixer theMixer;
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("Master Vol"))
+        {
+            theMixer.SetFloat("Master Vol", PlayerPrefs.GetFloat("Master Vol"));
+        }
+
+        if (PlayerPrefs.HasKey("Music Vol"))
+        {
+            theMixer.SetFloat("Music Vol", PlayerPrefs.GetFloat("Music Vol"));
+        }
+
+        if (PlayerPrefs.HasKey("SFX Vol"))
+        {
+            theMixer.SetFloat("SFX Vol", PlayerPrefs.GetFloat("SFX Vol"));
+        }
+    }
 
     private void Awake()
     {
--- RevengeGame/Assets/Scripts/UI Scripts/OptionsScreen.cs
+++ RevengeGame/Assets/Scripts/UI Scripts/OptionsScreen.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Audio;
 
 public class OptionsScreen : MonoBehaviour
 {
     public Toggle fullscreenTog, vsyncTog;
+    public List<ResItem> resolutions = new List<ResItem>();
+    private int selectedResolution;
+    public TMP_Text resolutionLabel;
+
+    public AudioMixer theMixer;
+    public TMP_Text mastLabel, musicLabel, sfxLabel;
+    public Slider mastSlider, musicSlider, sfxSlider;
 
     void Start()
     {
@@ -19,16 +28,46 @@ public class OptionsScreen : MonoBehaviour
         {
             vsyncTog.isOn = true;
         }
+
+        float vol = 0f;
+        theMixer.GetFloat("Master Vol", out vol);
+        mastSlider.value = vol;
+
+        theMixer.GetFloat("Music Vol", out vol);
+        musicSlider.value = vol;
+
+        theMixer.GetFloat("SFX Vol", out vol);
+        sfxSlider.value = vol;
+
+        mastLabel.text = Mathf.RoundToInt(mastSlider.value + 80).ToString();
+        musicLabel.text = Mathf.RoundToInt(musicSlider.value + 80).ToString();
+        sfxLabel.text = Mathf.RoundToInt(sfxSlider.value + 80).ToString();
     }
 
-    void Update()
+    public void ResLeft()
     {
-        
+        selectedResolution--;
+        if(selectedResolution < 0)
+        {
+            selectedResolution = 0;
+        }
+        UpdateResLabel();
     }
+
+    public void ResRight()
+    {
+        selectedResolution++;
+        if(selectedResolution > resolutions.Count - 1)
+        {
+            selectedResolution = resolutions.Count - 1;
+        }
+        UpdateResLabel();
+    }
+
 
     public void ApplyGraphics()
     {
-        Screen.fullScreen = fullscreenTog.isOn;
+        //Screen.fullScreen = fullscreenTog.isOn;
 
         if(vsyncTog.isOn)
         {
@@ -38,5 +77,42 @@ public class OptionsScreen : MonoBehaviour
         {
             QualitySettings.vSyncCount = 0;
         }
+
+        Screen.SetResolution(resolutions[selectedResolution].horizontal, resolutions[selectedResolution].vertical, fullscreenTog.isOn);
     }
+
+    public void UpdateResLabel()
+    {
+        resolutionLabel.text = resolutions[selectedResolution].horizontal.ToString() + " X " + resolutions[selectedResolution].vertical.ToString();
+    }
+
+    public void SetMasterVolume()
+    {
+        mastLabel.text = Mathf.RoundToInt(mastSlider.value + 80).ToString();
+        theMixer.SetFloat("Master Vol", mastSlider.value);
+
+        PlayerPrefs.SetFloat("Master Vol", mastSlider.value);
+    }
+
+    public void SetMusicVolume()
+    {
+        musicLabel.text = Mathf.RoundToInt(musicSlider.value + 80).ToString();
+        theMixer.SetFloat("Music Vol", musicSlider.value);
+
+        PlayerPrefs.SetFloat("Music Vol", musicSlider.value);
+    }
+
+    public void SetSFXVolume()
+    {
+        sfxLabel.text = Mathf.RoundToInt(sfxSlider.value + 80).ToString();
+        theMixer.SetFloat("SFX Vol", sfxSlider.value);
+
+        PlayerPrefs.SetFloat("SFX Vol", sfxSlider.value);
+    }
+}
+
+[System.Serializable]
+public class ResItem
+{
+    public int horizontal, vertical;
 }
