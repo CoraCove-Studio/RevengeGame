644c955851917869756f5746336a3c5e4b3632cc
ccurtis-waketech <kjcurtis@my.waketech.edu>
2023-10-08 19:57:36 -0400

    Misc Changes
    - Player damage is slightly random, picking from a small range around the provided damage (10 DMG).
    - CameraMovement script to have camera follow player.
    - Enemies will no longer permanently remain alive even after hitting 0 HP.
    - WIP pop-up that will appear showing damage numbers on screen when player deals damage.

--- RevengeGame/Assets/Scripts/PlayerScripts/AttackUniversal.cs
+++ RevengeGame/Assets/Scripts/PlayerScripts/AttackUniversal.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -12,6 +13,13 @@ public class AttackUniversal : MonoBehaviour
     public bool is_Player, is_Enemy;
 
     public GameObject hit_FX_Prefab;
+    private GameObject worldspaceCanvas;
+    public GameObject dmgPopUpPrefab;
+
+    private void Start()
+    {
+        //worldspaceCanvas = GameObject.Find("InGameUI_Canvas");
+    }
 
     // Update is called once per frame
     void Update()
@@ -28,6 +36,15 @@ public class AttackUniversal : MonoBehaviour
             if (is_Player)
             {
                 Vector3 hitFX_Pos = hit[0].transform.position;
+                //Debug.Log(hit[0].transform.position.x + " " + hit[0].transform.position.y + " " + hit[0].transform.position.z);
+                //GameObject popUp = Instantiate(dmgPopUpPrefab, Vector3.zero, Quaternion.identity);
+                //popUp.transform.parent = worldspaceCanvas.transform;
+                //Vector3 relativePos = Camera.main.transform.TransformPoint(GameObject.Find("Enemy").transform.position);
+                //Debug.Log(popUp.transform.position);
+                //popUp.transform.position = hit[0].transform.position;
+                //popUp.transform.position = relativePos;
+                //popUp.transform.position = new Vector3(Random.Range(relativePos.x - 0.3f, relativePos.x + 0.3f),
+                //    Random.Range(1.75f - 0.3f, 1.75f + 0.3f), Random.Range(relativePos.z - 0.3f, relativePos.z + 0.3f));
                 hitFX_Pos.y += 1.3f;
 
                 if (hit[0].transform.forward.x > 0)
--- RevengeGame/Assets/Scripts/UniversalScripts/HealthScript.cs
+++ RevengeGame/Assets/Scripts/UniversalScripts/HealthScript.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class HealthScript : MonoBehaviour
 {
-    public float health = 100f;
+    public int health = 100;
+    private GameObject dmgPopUp;
+    private bool invincible = false;
+
+    public bool randomDmg = true;
+    public bool randomHeal = true;
 
     private CharacterAnimation animationScript;
     private EnemyMovement enemyMovement;
@@ -26,8 +32,8 @@ public class HealthScript : MonoBehaviour
         {
             return;
         }
-
-        health -= damage;
+        if (randomDmg) { damage = Random.Range(damage * 0.75f, (damage * 1.25f) + 1); }
+        if (!invincible) { health -= (int)damage; }
 
         //display health ui
 
@@ -40,12 +46,25 @@ public class HealthScript : MonoBehaviour
             {
 
             }
+            else
+            {
+                Behaviour moveScript = gameObject.GetComponent<EnemyMovement>();
+                moveScript.enabled = false; // Needs to be disabled, otherwise enemy refuses to die.
+                CapsuleCollider collider = gameObject.GetComponent<CapsuleCollider>();
+                collider.excludeLayers = LayerMask.GetMask("Player"); // Stops the player from being able to collide with enemies on death.
+                //collider.direction = 2;
+                //collider.center = new Vector3(0, 0.2f, 1);
+            }
 
             return;
         }
+        else if (!invincible) { StartCoroutine(Invincible()); }
 
         if (!is_Player)
         {
+            //dmgPopUp = GameObject.Find("DMG-PopUp(Clone)");
+            //dmgPopUp.transform.GetChild(0).GetComponent<TMP_Text>().text = ((int)damage).ToString();
+
             if (knockDown)
             {
                 if (Random.Range(0, 2) > 0)
@@ -62,5 +81,14 @@ public class HealthScript : MonoBehaviour
                 }
             }
         }
+    }
+
+    // Entity becomes temporarily invincible when hit.
+    IEnumerator Invincible()
+    {
+        //invincible = true;
+        yield return new WaitForSeconds(2.0f);
+        // Play animation here?
+        invincible = false;
     }
 }
